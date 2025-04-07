22095ae9dadaefbf978639cef28f0dea89495c99
Alberto Sierra <alberto.sierra@murciaeduca.es>
2025-04-07 11:49:42 +0200

    class button primary a Crear patrocinador

--- resources/views/admin/patrocinadores/index.blade.php
+++ resources/views/admin/patrocinadores/index.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <a href="{{ route('patrocinadores.create') }}" class="btn btn-primary mb-4">Crear Patrocinador</a>
+                    <a href="{{ route('patrocinadores.create') }}" class="button primary">Crear Patrocinador</a>
                     <table class="table-auto w-full">
                         <thead>
                             <tr>
